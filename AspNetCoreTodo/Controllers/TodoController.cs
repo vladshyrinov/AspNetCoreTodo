3f0c3f4c1d69bc89a67196ad9583c93fd22c07c3
Vlad Shyrinov <vlad.shyrinov@gmail.com>
2019-08-28 08:37:15 +0200

    added project

---
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreTodo.Services;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly ITodoItemService _todoItemService;
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+        public async Task<IActionResult> Index() {
+            // Get to-do items from database
+
+            var items = await _todoItemService.GetIncompleteItemsAsync();
+
+            // Put items into a model
+
+            // Render view using the model
+        }
+    }
+}
